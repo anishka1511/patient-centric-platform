21cbfc4b3ec5e442186652708791d34a1d822973
Sumedh Jaltare <sumedhjaltare@Sumedhs-MacBook-Air.local>
2026-02-22 00:25:23 +0530

    deleted unnecessary files

--- run_test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-"""Quick wrapper to run test_quick.py without stdin issues"""
-import subprocess
-import sys
-
-result = subprocess.run([sys.executable, "test_quick.py"], capture_output=False)
-sys.exit(result.returncode)
